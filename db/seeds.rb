2f0f53584575666a1b7b8d65d142642da7e213e3
sdangol <dangol.shreejana@gmail.com>
2018-04-17 20:18:54 -0400

    Adding data in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |blog|
+	Blog.create!(
+		title: "My blog Post #{blog}",
+		body: "This is the body"
+	)
+end
+
+
+5.times do |skill|
+	Skill.create!(
+		title: "Rails #{skill}",
+		percent_utilized: 15
+	)
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfoio tile : #{portfolio_item}",
+		subtitle: "this is subtitle",
+		body: "This is the body",
+		main_image: "http://placehold.it/700X150",
+		thumb_image: "http://placehold.it/400X150ß"
+	)
+end
+puts "9 portfolios items created"
